1cfd174bef922c335a9aff2bc484e32aa1603609
Mayitzin <mayitzin@gmail.com>
2015-04-22 14:30:57 +0200

    Modified CSV reader and README file

--- README.md
+++ README.md
@@ -8,14 +8,20 @@ Useful Code for the several Robotics fields. It is divided into four major branc
 
 There is an extra folder **Data** to store the files used for examples and tests.
 
-The code is created using the Standard C99. For example, to compile a C file with GCC you can indicate this standard with:
+The code in C is compiled using the Standard C99. For example with the GCC you can indicate this standard with:
 
 ```
-gcc file.c -o output -std=c99
+gcc file.c -std=c99 -o output
 ```
 
 Unix systems may not find the files defining the called functions (although included in the code), so you must tell the linker to search for the required library using the -lm flag at the end of the command.
 
 ```
-gcc file.c -o output -std=c99 -lm
+gcc file.c -std=c99 -o output -lm
+```
+
+The code in C++ is compiled using the Standard C++11. For example with the GCC you can also indicate this standard as:
+
+```
+g++ file.cpp -std=c++11 -o output
 ```
--- Sensing/readcsv.cpp
+++ Sensing/readcsv.cpp
@@ -3,6 +3,7 @@
  * 
  * History:
  *     20.04.2015. First implementation.
+ *     22.04.2015. Using command line input to set parameters.
  *
  * @author: Mario Garcia
  * www.mayitzin.com
@@ -12,30 +13,43 @@
 #include <fstream>
 #include <string>
 
-#define FILE_LENGTH 5    // Max num of lines per file
+#define FILE_LENGTH 5           // Num of lines per file
+#define MAX_FILE_LENGTH 500     // Max num of lines per file
 
 using namespace std;
 
 int main(int argc, char* argv[]){
-    string line;
-    ifstream myfile ("../Data/some.csv");
+    string line, fileName;
+    int num_lines = 5;
+    ifstream csvFile;
 
     if (argc>1){
         cout << "File Name: " << argv[1] << endl;
-        ifstream myfile (argv[1]);
+        fileName = argv[1];
+        if (argc>2) {
+            num_lines = stoi(argv[2]);
+            if (num_lines>MAX_FILE_LENGTH){
+                num_lines = MAX_FILE_LENGTH;
+                cout << "A maximum of " << MAX_FILE_LENGTH << " are pemitted" << endl;
+            }
+        }
     } else {
-        cout << "Using default testing file" << endl;
+        cout << "Showing " << num_lines << " lines of the default testing file" << endl;
+        fileName = "../Data/circle.csv";
     }
 
-    // ifstream myfile ("../Data/circle.csv");
-    if (myfile.is_open()) {
+    csvFile.open(fileName.c_str());
+    if (csvFile.is_open()) {
         int i=0;
-        while (getline(myfile, line) && i<FILE_LENGTH) {
+        while (getline(csvFile, line) && i<num_lines) {
             cout << i << ": " << line << endl;
             i++;
         }
-        myfile.close();
+        csvFile.close();
+    } else {
+        cout << "[ERROR] Unable to open file" << endl;
+        cout << "\tUsage:\t./readcsv file.csv number_of_lines" << endl;
+        return -1;
     }
-    else cout << "Unable to open file" << endl;
     return 0;
 }
