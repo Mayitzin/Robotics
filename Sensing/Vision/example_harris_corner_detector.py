ac6a0d4d63d6046c6505cd6c8eb5185cd6a0fe71
Mario Garcia <mayitzin@gmail.com>
2015-06-27 00:45:35 +0200

    Automatic conversion to grayscale

--- Sensing/Vision/example_harris_corner_detector.py
+++ Sensing/Vision/example_harris_corner_detector.py
@@ -7,8 +7,17 @@ www.mayitzin.com
 
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+import numpy as np
 
-im = mpimg.imread('..\..\Data\harris.png')
-implot = plt.imshow(im)
+# Read Image
+im = mpimg.imread(r'..\..\Data\atlante.jpg')
+
+# Convert to grayscale if Image is RGB
+if np.shape(im)[2] >= 3:
+    r, g, b = im[:,:,0], im[:,:,1], im[:,:,2]
+    im = 0.2989*r + 0.5870*g + 0.1140*b
+
+# Show image in grayscale
+implot = plt.imshow(im, cmap='gray')
 
 plt.show()
