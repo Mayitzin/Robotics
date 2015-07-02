841c130fc0781ce3d2acd0574447a8eaecee7a46
Mario Garcia <mayitzin@gmail.com>
2015-07-02 18:14:21 +0200

    Added Structure Sensor constructor

--- Sensing/Vision/example_harris_corner_detector.py
+++ Sensing/Vision/example_harris_corner_detector.py
@@ -12,21 +12,50 @@ import numpy as np
 
 # Read Image
 im = mpimg.imread(r'..\..\Data\harris.png')
-# Convert to grayscale if Image is RGB
-if np.shape(im)[2] >= 3:
+M, N, C = np.shape(im)
+print "Y is", M, "\nX is", N
+# Convert to grayscale if Image is RGB-channel
+if C >= 3:
     r, g, b = im[:,:,0], im[:,:,1], im[:,:,2]
     im = 0.2989*r + 0.5870*g + 0.1140*b
 
-fim = scimg.filters.gaussian_filter(im, 3, order=1)
+# Convolution of Image with derivatives of Gaussians (Laplacians)
+Lx = scimg.filters.gaussian_filter1d(im, 1, axis=1, order=1)
+Ly = scimg.filters.gaussian_filter1d(im, 1, axis=0, order=1)
+
+# Elements of Structure Tensor Matrix
+Lx2 = Lx**2
+Ly2 = Ly**2
+Lxy = Lx*Ly
+
+# Compute the Structure Tensor Matrix
+alpha = 0.15
+# t = -0.0035
+R = np.zeros((M,N))
+for y in range(M):
+	for x in range(N):
+		S = np.array([[Lx2[y,x], Lxy[y,x]], [Lxy[y,x], Ly2[y,x]]])
+		R[y,x] = np.linalg.det(S) - alpha*np.trace(S)**2
+		# if R[y,x]>t:
+		# 	R[y,x] = 0.0
 
 # Show images in grayscale
-plt.subplot(1,2,1)
+plt.subplot(2,2,1)
 implot = plt.imshow(im, cmap='gray')
 plt.title('Original Image')
 plt.axis('off')
-plt.subplot(1,2,2)
-fimplot = plt.imshow(fim, cmap='gray')
-plt.title('Filtered Image')
+plt.subplot(2,2,2)
+Lx_plot = plt.imshow(Lx, cmap='gray')
+plt.title('Filtered X-axis')
+plt.axis('off')
+plt.subplot(2,2,3)
+Ly_plot = plt.imshow(Ly, cmap='gray')
+plt.title('Filtered Y-axis')
+plt.axis('off')
+plt.subplot(2,2,4)
+R_plot = plt.imshow(R, cmap='gray')
+plt.colorbar(R_plot)
+plt.title('Structure Tensor')
 plt.axis('off')
 
 
