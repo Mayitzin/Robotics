8f7f7d2fe9083cc8a7fe45ee6c7751ddcb16cdfa
Mario Garcia <mayitzin@gmail.com>
2016-02-12 01:36:41 +0100

    Added KF function

--- Motion/kf_example.py
+++ Motion/kf_example.py
@@ -11,5 +11,29 @@ www.mayitzin.com
 """
 
 import numpy as np
+import scipy.linalg as lin
+
+
+def kf(xhat, z, A, P, Q, R, H):
+    """
+    The linear Kalman Filter computes the current state m-by-1 vector
+    xhat and its m-by-m Covariance matrix P, given the parameters of the
+    previous state.
+        m is the number of elements in array xhat.
+        n is the length of array z (number of sensor signals to compare).
+    """
+    m = len(xhat)
+    n = len(z)
+    I = np.eye(m)
+    # KF - Prediction
+    xhat = np.dot(A, xhat)
+    P    = np.dot(A, np.dot(P, A.T)) + Q
+    # KF - Update
+    S    = np.dot(H, np.dot(P, H.T)) + R
+    K    = np.dot(P, np.dot(H.T, lin.inv(S)))
+    v    = z.reshape((n,1)) - np.dot(H, xhat)
+    xhat = xhat + np.dot(K, v)
+    P    = np.dot((I - np.dot(K, H)), P)
+    return xhat, P
 
 print "First Draft"
