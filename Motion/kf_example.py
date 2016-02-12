daebbc484b65e3c7488c15e9d4dd6b2a7ed00ccb
Mario Garcia <mayitzin@gmail.com>
2016-02-12 01:31:00 +0100

    Added script to compare KF results

---
+++ Motion/kf_example.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+This script plots the results of a Kalman Filter with different parameter
+values.
+
+History:
+    12.02.2016. First Implementation.
+
+@author: Mario Garcia
+www.mayitzin.com
+"""
+
+import numpy as np
+
+print "First Draft"
