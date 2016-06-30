a9f474d2609fb1e8298e2fcd5d0e24e057cef5cd
Mario Garcia <mayitzin@gmail.com>
2016-06-30 23:40:53 +0200

    Add sigmoid, softmax and easier bernoulli

--- Intelligence/mllib.py
+++ Intelligence/mllib.py
@@ -26,24 +26,17 @@ def ber(X, p=0.5):
 
     f(k | p) = p^k * (1-p)^(1-k)        for k in {0,1}
 
-    This function computes the distribution with both methods
-
     see:
     - https://en.wikipedia.org/wiki/Bernoulli_distribution
     - http://mathworld.wolfram.com/BernoulliDistribution.html
     """
-    f1 = np.zeros(np.shape(X))  # Result using first method
-    f2 = np.zeros(np.shape(X))  # Result using second method
-    for k in range(len(X)):
-        if X[k]==0:   f1[k] = 1.0-p
-        elif X[k]==1: f1[k] = p
-        f2[k] = p**X[k] * (1.0-p)**(1-X[k])
+    f = p**X * (1.0-p)**(1-X)
     # Get Properties of the distribution
     mean = p
     var = p*(1.0-p)
     std = np.sqrt(var)
     # Return the values
-    return f1, f2, mean, var, std
+    return f, mean, var, std
 
 
 def pdf(X, m=0.0, s=1.0):
@@ -70,6 +63,10 @@ def pdf(X, m=0.0, s=1.0):
     return f
 
 
+def sigmoid(x):
+    return 1./(1+np.exp(-x))
+
+
 def softmax(z):
     return np.exp(z)/np.sum(np.exp(z))
 
@@ -84,8 +81,8 @@ def test_ber(n=5,p=0.5):
     print "------------------------------"
     X = np.random.random_integers(0,1,size=(n))
     print "X = %d random sampled outcomes (Bernoulli trials) =\n"%(n), X
-    f1,_, mean, var, std = ber(X,p)
-    print "\nP(X ; %.2f) =\n"%(p), f1, "\n"
+    f, mean, var, std = ber(X,p)
+    print "\nP(X ; %.2f) =\n"%(p), f, "\n"
     print "  mean = %.2f"%mean
     print "  var = %.3f"%var
     print "  std = %.4f"%std
@@ -95,7 +92,8 @@ def test_ber(n=5,p=0.5):
 
 ###### Testing the Functions ######
 
-test_ber(5,0.3)
+p = 0.3
+test_ber(5,p)
 
 # X = np.linspace(-4.0, 4.0, num=40)
 # Y = pdf(X)
